72da63f36550377e25b55a1d7a3919447dc92abb
Artemis <wzyzhouyi@gmail.com>
2020-09-18 22:45:21 -0700

    fixed local storage not saving properly issue

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
 
-    // Display current date and hour
+    // Display current date
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
-    var currentHour = 0;
-    setInterval(function() {
-        currentHour = moment().format("k");
-    }, 10000) 
 
-    // In a for loop, create one row. time: 9-17 
+    var currentHour = moment().format("k");
+
+    var timeInterval = setInterval(function () {
+        // console.log(currentHour);
+        if (currentHour < 9 || currentHour > 17) {
+            clearInterval(timeInterval);
+        }
+    }, 30000);
+
 
     for (var i = 9; i <= 17; i++) {
-        var numBtn = "btn" + i;
-        var newRow = $("<section>", {class: "row time-block"});
+        var newRow = $("<section>", { class: "row time-block" });
         var time = $("<span>");
 
         if (i <= 11) {
@@ -24,42 +27,75 @@ $(document).ready(function () {
             time.text(i - 12 + "PM");
         }
 
-        var colTime = $("<section>", {class: "col-md-1 hour"}).append(time);
-        var colText = $("<textarea>", {class: "col-md-10 description"});
-        var colSave = $("<button>", {id: numBtn, class: "col-md-1 saveBtn"}).append($("<i>", {class: "fas fa-save"}));
+        var colTime = $("<section>", { class: "col-md-1 hour" }).append(time);
+        var colText = $("<textarea>", { class: "col-md-10 description" });
+        var colSave = $("<button>", { id: "btn" + i, class: "col-md-1 saveBtn" }).append($("<i>", { id: "icon" + i, class: "fas fa-save" }));
+
+        colText.text(JSON.parse(localStorage.getItem("btn" + i)));
 
         newRow.append([colTime, colText, colSave]);
         $(".container").append(newRow);
-
+    
     }
 
-    // console.log("#btn" + currentHour);
-    if (currentHour >= 9) {
-        $(".container").find("#btn" + currentHour).prev().css("background-color", "#EED5D2");
-        for (var j = 9; j < currentHour; j++) {
-            $("#btn" + j).prev().css("background-color", "#EBEBEB");
+    // console.log(typeof currentHour);
+    if (parseInt(currentHour) >= 9) {
+        $(".container").find("#btn" + currentHour).prev().css("background-color", "#EED5D2"); // pale red
+        for (var j = 9; j < parseInt(currentHour); j++) {
+            $("#btn" + j).prev().css("background-color", "#EBEBEB"); // pale gray
         }
-    
-        for (var k = currentHour + 1; k <= 17; k++) {
+
+        for (var k = parseInt(currentHour) + 1; k <= 17; k++) {
             $("#btn" + k).prev().css("background-color", "#CFDBC5");
         }
     } else {
-        $("textarea").css("background-color", "#CFDBC5"); 
+        $("textarea").css("background-color", "#CFDBC5"); //pale green
     }
 
+    // localStorage.getItem()
     // Check whether it's a button being clicked
     function saveText(event) {
         event.preventDefault();
+        
+        // console.log($(event.target).is("i"));
+        if ($(event.target).is("button") || $(event.target).is("i")) {
+            var arrayText = [];
+            var inputText = "";
+            var numBtn = 0;
+            var buttonClicked = "";
+            var idClicked = event.target.id;
+            numBtn = idClicked[idClicked.length - 1];
+            buttonClicked = "#btn" + numBtn;
+            
+            console.log("event target id: " + event.target.id);
+            console.log(idClicked.length);
+            console.log(idClicked[idClicked.length - 1]);
+            console.log("numBtn: " + numBtn);
+            console.log("buttonClicked: " + buttonClicked);
+            console.log("idClicked: " + idClicked);
+            arrayText = JSON.parse(localStorage.getItem("btn" + numBtn)) || [];
+            inputText = $(buttonClicked).prev().val();
+            // console.log(typeof arrayText);
+            // console.log(arrayText);
+            // console.log(inputText);
+            if (inputText) {
+                arrayText.push(inputText);
+                localStorage.setItem("btn" + numBtn, JSON.stringify(arrayText));
+            }
+            console.log(localStorage.getItem("btn" + numBtn));
 
-        if ($(event.target).is("button")) {
-            // console.log(event.target.id);
-            // saveText(event.target.id);
-            var buttonClicked = "#" + event.target.id;
-            localStorage.setItem(event.target.id, $(buttonClicked).prev().val());
+            var displayArray = JSON.parse(localStorage.getItem("btn" + numBtn));
+            console.log(displayArray);
+            $(buttonClicked).prev().text(displayArray);
+            // displayArray.forEach(function(textEl) {
+            //     var newLine = $("<li>").text(textEl);
+            //     console.log(newLine);
+            //     $(buttonClicked).prev().append(newLine);
+            // });
         }
     }
 
-    $(this).on("click", saveText);
+    $(document).on("click", saveText);
 
 });
 
