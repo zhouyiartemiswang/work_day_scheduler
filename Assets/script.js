b0eb3401bb0b0f23b11f6a7b13193d3a7680558e
Artemis <wzyzhouyi@gmail.com>
2020-09-18 23:18:05 -0700

    added comment

--- Assets/script.js
+++ Assets/script.js
@@ -3,16 +3,20 @@ $(document).ready(function () {
     // Display current date
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-    var currentHour = moment().format("k");
+    // Retrieve current hour and convert to an integer
+    var currentHour = parseInt(moment().format("k"));
 
+    // Website refreshes every 30 seconds, so the current hour will be properly updated
     var timeInterval = setInterval(function () {
-        // console.log(currentHour);
+
+        // If user access website outside hours of 9AM - 5PM, the website will not automatically check hour
         if (currentHour < 9 || currentHour > 17) {
             clearInterval(timeInterval);
         }
+
     }, 30000);
 
-
+    // Dynamically add each row of 3 columns
     for (var i = 9; i <= 17; i++) {
         var newRow = $("<section>", { class: "row time-block" });
         var time = $("<span>");
@@ -31,59 +35,61 @@ $(document).ready(function () {
         var colText = $("<textarea>", { class: "col-md-10 description" });
         var colSave = $("<button>", { id: "btn" + i, class: "col-md-1 saveBtn" }).append($("<i>", { id: "icon" + i, class: "fas fa-save" }));
 
+        // Print out previously stored data to <textarea>
         colText.text(JSON.parse(localStorage.getItem("btn" + i)));
 
         newRow.append([colTime, colText, colSave]);
         $(".container").append(newRow);
-    
+
     }
 
-    // console.log(typeof currentHour);
-    if (parseInt(currentHour) >= 9) {
+    // Set background color of textarea based on current hour
+    // Past hour - pale gray, current hour - pale red, future hour - pale green
+    if (currentHour >= 9) {
         $(".container").find("#btn" + currentHour).prev().css("background-color", "#EED5D2"); // pale red
         for (var j = 9; j < parseInt(currentHour); j++) {
             $("#btn" + j).prev().css("background-color", "#EBEBEB"); // pale gray
         }
 
         for (var k = parseInt(currentHour) + 1; k <= 17; k++) {
-            $("#btn" + k).prev().css("background-color", "#CFDBC5");
+            $("#btn" + k).prev().css("background-color", "#CFDBC5"); // pale green
         }
     } else {
-        $("textarea").css("background-color", "#CFDBC5"); //pale green
+        $("textarea").css("background-color", "#CFDBC5"); // pale green
     }
 
     // Check whether it's a button being clicked
     function saveText(event) {
+
         event.preventDefault();
-        
-        // console.log($(event.target).is("i"));
+
+        // Make sure only clicking on a button can trigger the event
         if ($(event.target).is("button") || $(event.target).is("i")) {
-            
+
             var arrayText = [];
             var inputText = "";
-            var numBtn = event.target.id.split("n")[1];
-            var buttonClicked = "btn" + numBtn;
-            
-            console.log("event target id: " + event.target.id);
-            console.log("numBtn: " + numBtn);
-            console.log("buttonClicked: " + buttonClicked);
+            var numBtn = event.target.id.split("n")[1]; // Get the number in the id
+            var buttonClicked = "btn" + numBtn; // Form button id
+
             arrayText = JSON.parse(localStorage.getItem(buttonClicked)) || [];
-            inputText = $("#" + buttonClicked).prev().val();
-    
+            inputText = $("#" + buttonClicked).prev().val(); // Get user input
+
+            // Make sure input is not empty
             if (inputText) {
+
+                // Add input to array then save to local storage
                 arrayText.push(inputText);
                 localStorage.setItem(buttonClicked, JSON.stringify(arrayText));
-            }
-            console.log(localStorage.getItem(buttonClicked));
 
-            var displayArray = JSON.parse(localStorage.getItem(buttonClicked));
-            console.log(displayArray);
-            $("#" + buttonClicked).prev().text(displayArray);
-    
-        }
+                // Display updated text in textarea
+                var displayArray = JSON.parse(localStorage.getItem(buttonClicked));
+                $("#" + buttonClicked).prev().text(displayArray);
+
+            }
+        } 
     }
 
+    // Save text, click event 
     $(document).on("click", saveText);
 
 });
-
